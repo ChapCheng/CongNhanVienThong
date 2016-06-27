3826fdc127a81e1469f10562145bfbeb74331c03
ChapCheng <thinguyen.219@gmail.com>
2016-06-27 14:49:36 +0700

    thinp

---
+++ src/com/congnhanvienthong/dhsc/ActivityTraCuuVissa.java
@@ -0,0 +1,14 @@
+package com.congnhanvienthong.dhsc;
+
+import com.congnhanvienthong.ActivityBaseToDisplay;
+
+import android.os.Bundle;
+
+public class ActivityTraCuuVissa extends ActivityBaseToDisplay {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
